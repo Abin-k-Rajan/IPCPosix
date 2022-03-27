1c43ca19635990bbf4e94c912b7e5fb9ba089f66
Abin-k-Rajan <abin27577@gmail.com>
2022-03-27 23:40:55 +0530

    version 1.0.1

--- MYLASTNAME_ID.c
+++ MYLASTNAME_ID.c
@@ -4,76 +4,65 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <semaphore.h>
-#include <pthread.h>
+#include <string.h>
 
-#define SEM_MUTEX   "mutex" 		//	SEMAPHORE NAMES FOR THREAD AND PROCESS SYNCHRONIZATION
-#define SEM_NEMPTY  "nempty"
-#define SEM_NSTORED "nstored"
-
-char *ptr1, *ptr2;			//	POINTER TO SHARED MEM ( CREATED BY MMAP SYSTEM CALL )
-struct {
-	sem_t *mutex, *nempty, *nstored;	//	SEMAPHORE
-} shared;
-
+char *ptr1, *ptr2;
+int* in, *out, *done, output_file;
 
 char* SRCFILENAME = "test.txt";			//	GLOBAL VARIABLES FOR PARENT AND CHILD PROCESS
 char* OUTFILENAME = "test.out";
 int CHUNK_SIZE = 10;
 int BUFFER_SIZE = 10;
 int NBUFF = 10;					//	NUMBER OF CHUNKS THAT CAN BE ACCOMODATED IN BUFFER
+int fd_pipe[2];
+char* OUTPUTFILENAME = "MYLASTNAME_ID.out";
+
+void produce();
+void consume();
 
 
-						
-void *produce(void *), *consume(void *);		//	FUNCTION POINTERS TO PRODUCE AND CONSUME DATA FROM / TO SHARED MEMORY
+char* getOutputFileName(char* name, char* extension);
+int getFileNameSizeWithOutExtension(char* name);
 
-void zeroBytes(int size);				//	FUNCTION TO SET BYTES IN CHUNK AS ZERO
-int maxChunkSize(int start, int curr);			//	CHECKING MAXIMUM AVAILABLE CLUSTER SIZE IN BUFFER AFTER EACH OPERATION	
-char* readChunk();					//	READ DATA FROM EACH CHUNK
-int getSizeOfBytes(char* buff);				//	UTILITY FUNCTION TO GET SIZE OF BYTES READ FROM SHARED MEMORY
-int writeChunkToOutputFile(int fd, char* buff);		//	FUNCTION TO WRITE OUTPUT TO OUTPUT FILE
-char* getCharPointerFromArray(char buff[], int n);	//	UTILITY FUNCTION TO CONVERT CHAR[] TO CHAR*
+int getBufferSize(char* buff);
+void outputToTerminal(int who, char* buff, int index);
+void writeOutput(char* buff, int len);
+int countDigits(int num);
+char* numberToChar(int num);
+int getBufferSize(char* buff);
+
+void printBytesWritten(char* content,int producerBytes);
+void outputToTerminal(int who, char* buff, int index);
+
 
 
 int main(int argc, char** argv)
 {
-	pthread_t producer, consumer;
 	int fd;
 	pid_t pid;
-	void* address;
-	int fd1, fd2;
-	
-	int flags = O_EXCL | O_CREAT;
-
-	if (argc != 5)
-	{
-		printf(" ERROR\n RUN PROGRAM AS FOLLOWS\n ./MYLASTNAME_ID.exe SRC_FILE CHUNK_SIZE OUT_FILE BUFFER_SIZE\n");
-		exit(-2);
-	}
-
 	SRCFILENAME = argv[1];
 	CHUNK_SIZE = atoi(argv[2]);
 	OUTFILENAME = argv[3];
 	BUFFER_SIZE = atoi(argv[4]);
+	int flags = O_EXCL | O_CREAT;
+	void* address;
 
-	sem_unlink(SEM_MUTEX);		//	UNLINK SEMAPHORE AND SHARED MEMORY AS A MEASURE TO CLOSE PREVIOUSLY OPENED FILE WITH SAME NAME
-	sem_unlink(SEM_NEMPTY);
-	sem_unlink(SEM_NSTORED);
+	OUTPUTFILENAME = getOutputFileName(argv[0], "out");
+	
+	output_file = open(OUTPUTFILENAME, O_RDWR | O_APPEND | O_CREAT, S_IRWXU);
+
+	pipe(fd_pipe);
+
 	shm_unlink(SRCFILENAME);
 
-	NBUFF = BUFFER_SIZE / CHUNK_SIZE;
-
-	shared.mutex = sem_open(SEM_MUTEX, flags, S_IRWXU, 1);		//	MUTEX
-	shared.nempty = sem_open(SEM_NEMPTY, flags, S_IRWXU, NBUFF);	//	COUNTING SEMAPHORE
-	shared.nstored = sem_open(SEM_NSTORED, flags, S_IRWXU, 0);	//	COUNTING SEMAPHORE
-
-
-	fd1 = shm_open(SRCFILENAME, flags | O_RDWR, S_IRWXU);		//	OPENING SHARED MEMORY
-	ftruncate(fd1, BUFFER_SIZE);
+	fd = shm_open(SRCFILENAME, flags | O_RDWR, S_IRWXU);		//	OPENING SHARED MEMORY
 	
-	address = (void *)malloc(BUFFER_SIZE);		//	ADDRESS THAT IS USED FOR MAPPING IN MMAP FUNCTION
+	ftruncate(fd, BUFFER_SIZE + 12);		//	EXTRA 2 BYTES FOR VARS IN AND OUT (SYNCHRONIZATION)
+
+	address = (void *)malloc(BUFFER_SIZE + 12);		//	ADDRESS THAT IS USED FOR MAPPING IN MMAP FUNCTION
 	
 	pid = fork();		//	FORK() CREATING CHILD AND PARENT
+
 
 	if (pid < 0)
 	{
@@ -81,151 +70,244 @@ int main(int argc, char** argv)
 	}
 	if (pid == 0)
 	{
-		ptr2 = mmap(address, BUFFER_SIZE, PROT_READ, MAP_SHARED, fd1, 0);		//	CONSUMER CODE
-		pthread_create(&consumer, NULL, consume, NULL);
-		pthread_join(consumer, NULL);
-		fd2 = shm_open(SRCFILENAME, flags | O_RDONLY, S_IRWXU);
+		ptr2 = mmap(address, BUFFER_SIZE + 12, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);		//	CONSUMER CODE
+		close(fd_pipe[1]);
+		consume();
 		exit(0);
 	}
 	
-	ptr1 = mmap(address, BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);		//	PRODUCER CODE
-	pthread_create(&producer, NULL, produce, NULL);
-	pthread_join(producer, NULL);
-
-
-	sem_unlink(SEM_MUTEX);		// 	UNLINKING ALL THE FILES
-	sem_unlink(SEM_NEMPTY);
-	sem_unlink(SEM_NSTORED);
+	ptr1 = mmap(address, BUFFER_SIZE + 12, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);		//	PRODUCER CODE
+	close(fd_pipe[0]);	
+	produce();	
 	shm_unlink(SRCFILENAME);
 	return 0;
 }
 
 
-void* produce(void* arg)
+void produce()
 {
-	char* ref = ptr1;
-	int produced_count = 0;
-	int i;
+	in = ptr1 + BUFFER_SIZE;
+	out = ptr1 + BUFFER_SIZE + 4;
+	done = ptr1 + BUFFER_SIZE + 8;
+	*in = 0;
+	*out = 0;
+	*done = 0;
+	int iters = 1;
+	int shMemProducerCharCnt = 0;
+
+	char* buff = (char*)malloc(CHUNK_SIZE);
+
 	int fd = open(SRCFILENAME, O_RDONLY);
-	char buff[CHUNK_SIZE];
 	int read_bytes = read(fd, buff, CHUNK_SIZE);
+
 	while (read_bytes > 0)
 	{
-		sem_wait(shared.nempty);
-		sem_wait(shared.mutex);
-
-		int cluster_size = maxChunkSize((int)ref, (int)ptr1);
-		zeroBytes(CHUNK_SIZE);
-
-		for (int j = 0; j < read_bytes; j++)	
-			*ptr1++ = buff[j];
-
-		if (cluster_size < CHUNK_SIZE)
+		*(buff + read_bytes) = 0x00;
+		for (int i = 0; i < read_bytes; i++)
 		{
-			ptr1 = ref;
-			cluster_size = CHUNK_SIZE;
+			while ((*in + 1) % BUFFER_SIZE == *out)
+			{
+
+			}
+			*(ptr1 + *in) = (char)(*buff++);
+			*in = (*in + 1) % BUFFER_SIZE;
+			shMemProducerCharCnt++;
 		}
-		produced_count++;
-		
-		char* str = getCharPointerFromArray(buff, read_bytes);
-		printf("PARENT: IN = %d\n", produced_count);
-		printf("PARENT: ITEM = %s\n", str);
-		
-		read_bytes = read(fd, buff, cluster_size);
-		sem_post(shared.mutex);
-		sem_post(shared.nstored);
+		buff -= read_bytes;
+		outputToTerminal(0, buff, iters++);
+		read_bytes = read(fd, buff, CHUNK_SIZE);
 	}
+	*done = 1;
+	write(fd_pipe[1], &shMemProducerCharCnt, sizeof(int));
+	printBytesWritten("PRODUCER: The producer value of shMemProducerCharCnt = ", shMemProducerCharCnt);
+	close(fd);
 }
 
 
-void* consume(void* arg)
+void readFromPipe(int consumerCharCount)
 {
-	int i;
+	int c;
+	read(fd_pipe[0], &c, sizeof(int));
+	printBytesWritten("CONSUMER: The producer value of shMemProducerCharCnt = ", c);
+	printBytesWritten("CONSUMER: The producer value of shMemConsumerCharCnt = ", consumerCharCount);	
+}
+
+
+void consume()
+{
+	in = ptr2 + BUFFER_SIZE;
+	out = ptr2 + BUFFER_SIZE + 4;
+	done = ptr2 + BUFFER_SIZE + 8;
+	int shMemConsumerCharCnt = 0;
+	int iters = 1;
+
+	int fd = open(OUTFILENAME, O_CREAT | O_WRONLY);
+
+	char* buff = (char*) malloc(CHUNK_SIZE);
 	int count = 0;
-	char* ref = ptr2;
-	int fd = open(OUTFILENAME, O_WRONLY | O_CREAT, S_IRWXU);
+	
 	for (;;)
 	{
-		sem_wait(shared.nstored);
-		sem_wait(shared.mutex);
-		int cluster_size = maxChunkSize((int) ref, (int) ptr2);
-		char* buff = readChunk();
-		writeChunkToOutputFile(fd, buff);		
-
-		if (cluster_size < CHUNK_SIZE)
+		while (*in == *out)
 		{
-			ptr2 = ref;
+			if (*done == 1)
+			{	
+				buff -= count;
+				outputToTerminal(1, buff, iters);
+				int res = write(fd, buff, count);
+				if (res < 0)
+				{
+					exit(-2);
+				}
+				readFromPipe(shMemConsumerCharCnt);
+				close(fd);
+				return;	
+			}
 		}
-
+		*buff++ = *(ptr2 + *out);
 		count++;
-		printf("CHILD: OUT = %d\n", count);
-		printf("CHILD: ITEM = %s\n", buff);
-		sem_post(shared.mutex);
-		sem_post(shared.nempty);
-	}
-}
-
-
-
-char* getCharPointerFromArray(char buff[], int n)
-{
-	char* buffer = (char*) malloc(n);
-	for (int i = 0; i < n; i++)
-	{
-		*buffer++ = buff[i];
-	}
-	buffer -= n;
-	return buffer;
-}
-
-
-void zeroBytes(int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		*(ptr1 + i) = 0x00;
-	}
-}
-
-
-int maxChunkSize(int start, int curr)
-{
-	int diff = (start + BUFFER_SIZE) - curr;
-	return (diff < CHUNK_SIZE ? diff : CHUNK_SIZE);
-}
-
-
-
-char* readChunk()
-{
-	char* buff = (char *)malloc(CHUNK_SIZE);
-	int count = 0;
-	while (*ptr2 != 0)
-	{
+		shMemConsumerCharCnt++;
 		if (count == CHUNK_SIZE)
-			break;
-		*buff++ = *ptr2++;
-		count++;
+		{
+			buff -= count;
+			outputToTerminal(1, buff, iters++);
+			
+			int res = write(fd, buff, count);
+			if (res < 0)
+			{
+				exit(-2);
+			}
+			count = 0;
+			buff = (char *)malloc(CHUNK_SIZE);
+		}
+		*out = (*out + 1) % BUFFER_SIZE;
 	}
-	buff -= count;
-	return buff;
+
 }
 
-int getSizeOfBytes(char* buff)
+
+	//	UTILITY FUNCTION DEFINITION STARTS HERE
+
+
+int getFileNameSizeWithOutExtension(char* name)
+{
+	int size = getBufferSize(name);
+	for (int i = 1; i < size; i++)
+	{
+		if (*(name + i) == '.')
+			return i;
+	}
+	return size;
+}
+
+
+char* getOutputFileName(char* name, char* extension)
+{
+	int nameSize = getFileNameSizeWithOutExtension(name);
+	printf("%d\n", nameSize);
+	int extensionSize = getBufferSize(extension);
+	int totalSize = nameSize + extensionSize + 1;
+	
+	char* nameWithExtension = (char *)malloc(totalSize);
+	nameWithExtension = strncat(nameWithExtension, name, nameSize);
+	nameWithExtension = strcat(nameWithExtension, ".");
+	nameWithExtension = strcat(nameWithExtension, extension);
+
+	return nameWithExtension;
+}
+
+
+void writeOutput(char* buff, int len)
+{
+	write(STDOUT_FILENO, buff, len);
+	write(output_file, buff, len);
+}
+
+
+int countDigits(int num)
 {
 	int count = 0;
-	while (*(buff + count) != 0)
+	while (num)
 	{
-		count++;
+		count += 1;
+		num /= 10;
 	}
 	return count;
 }
 
 
-int writeChunkToOutputFile(int fd, char* buff)
+
+char* numberToChar(int num)
 {
-	int res = write(fd, buff, getSizeOfBytes(buff));
-	if (res < 0)
-		return -1;
-	return 0;
+	int size = countDigits(num);
+	char* buff = (char *)malloc(size);
+	for(int i = 0; i < size; i++)
+	{
+		*(buff + size - i - 1) = (char)(num % 10 + 48);
+		num /= 10;
+	}
+	return buff;
 }
+
+int getBufferSize(char* buff)
+{
+	int size = 0;
+	while (*(buff + size) != 0)
+	{
+		size++;
+	}
+	return size;
+}
+
+
+void printBytesWritten(char* content,int producerBytes)
+{
+	//char* content = "PRODUCER: The producer value of shMemProducerCharCnt = ";
+	int bufferSize = getBufferSize(content);
+	char* number = numberToChar(producerBytes);
+	int digitsCount = countDigits(producerBytes);
+
+	int totalBufferSize = bufferSize + digitsCount + 1;
+
+	char* buffer = (char*)malloc(totalBufferSize);
+
+	buffer = strcat(buffer, content);
+	buffer = strcat(buffer, number);
+	buffer = strcat(buffer, "\n");
+
+	writeOutput(buffer, totalBufferSize);
+}
+
+
+void outputToTerminal(int who, char* buff, int index)
+{
+	char* parent = "PARENT: ";
+	char* child = "CHILD: ";
+
+	char* _in = "IN = ";
+	char* _item = "ITEM = ";
+	char* number = numberToChar(index);
+
+	int digitsCount = countDigits(index);
+	int bufferSize = getBufferSize(buff);
+
+
+	int buff1Size = getBufferSize((who == 0 ? parent : child)) + getBufferSize(_in) + digitsCount + 1;
+	int buff2Size = getBufferSize((who == 0 ? parent : child)) + getBufferSize(_item) + bufferSize + 1;
+
+	int totalBuffSize = buff1Size + buff2Size;
+
+	char* buff1 = (char *)malloc(totalBuffSize);
+
+	buff1 = strcat(buff1, (who == 0 ? parent : child));
+	buff1 = strcat(buff1, _in);
+	buff1 = strcat(buff1, number);
+	buff1 = strcat(buff1, "\n");
+
+	buff1 = strcat(buff1, (who == 0 ? parent : child));
+	buff1 = strcat(buff1, _item);
+	buff1 = strcat(buff1, buff);
+	buff1 = strcat(buff1, "\n");
+	writeOutput(buff1, totalBuffSize);
+}
+
--- producer.c
+++ producer.c
@@ -7,17 +7,32 @@
 #include <string.h>
 
 char *ptr1, *ptr2;
-int* in, *out, *done;
+int* in, *out, *done, output_file;
 
 char* SRCFILENAME = "test.txt";			//	GLOBAL VARIABLES FOR PARENT AND CHILD PROCESS
 char* OUTFILENAME = "test.out";
 int CHUNK_SIZE = 10;
 int BUFFER_SIZE = 10;
 int NBUFF = 10;					//	NUMBER OF CHUNKS THAT CAN BE ACCOMODATED IN BUFFER
-
+int fd_pipe[2];
+char* OUTPUTFILENAME = "MYLASTNAME_ID.out";
 
 void produce();
 void consume();
+
+
+char* getOutputFileName(char* name, char* extension);
+int getFileNameSizeWithOutExtension(char* name);
+
+int getBufferSize(char* buff);
+void outputToTerminal(int who, char* buff, int index);
+void writeOutput(char* buff, int len);
+int countDigits(int num);
+char* numberToChar(int num);
+int getBufferSize(char* buff);
+
+void printBytesWritten(char* content,int producerBytes);
+void outputToTerminal(int who, char* buff, int index);
 
 
 
@@ -31,6 +46,12 @@ int main(int argc, char** argv)
 	BUFFER_SIZE = atoi(argv[4]);
 	int flags = O_EXCL | O_CREAT;
 	void* address;
+
+	OUTPUTFILENAME = getOutputFileName(argv[0], "out");
+	
+	output_file = open(OUTPUTFILENAME, O_RDWR | O_APPEND | O_CREAT, S_IRWXU);
+
+	pipe(fd_pipe);
 
 	shm_unlink(SRCFILENAME);
 
@@ -50,14 +71,156 @@ int main(int argc, char** argv)
 	if (pid == 0)
 	{
 		ptr2 = mmap(address, BUFFER_SIZE + 12, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);		//	CONSUMER CODE
+		close(fd_pipe[1]);
 		consume();
 		exit(0);
 	}
 	
 	ptr1 = mmap(address, BUFFER_SIZE + 12, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);		//	PRODUCER CODE
+	close(fd_pipe[0]);	
 	produce();	
 	shm_unlink(SRCFILENAME);
 	return 0;
+}
+
+
+void produce()
+{
+	in = ptr1 + BUFFER_SIZE;
+	out = ptr1 + BUFFER_SIZE + 4;
+	done = ptr1 + BUFFER_SIZE + 8;
+	*in = 0;
+	*out = 0;
+	*done = 0;
+	int iters = 1;
+	int shMemProducerCharCnt = 0;
+
+	char* buff = (char*)malloc(CHUNK_SIZE);
+
+	int fd = open(SRCFILENAME, O_RDONLY);
+	int read_bytes = read(fd, buff, CHUNK_SIZE);
+
+	while (read_bytes > 0)
+	{
+		*(buff + read_bytes) = 0x00;
+		for (int i = 0; i < read_bytes; i++)
+		{
+			while ((*in + 1) % BUFFER_SIZE == *out)
+			{
+
+			}
+			*(ptr1 + *in) = (char)(*buff++);
+			*in = (*in + 1) % BUFFER_SIZE;
+			shMemProducerCharCnt++;
+		}
+		buff -= read_bytes;
+		outputToTerminal(0, buff, iters++);
+		read_bytes = read(fd, buff, CHUNK_SIZE);
+	}
+	*done = 1;
+	write(fd_pipe[1], &shMemProducerCharCnt, sizeof(int));
+	printBytesWritten("PRODUCER: The producer value of shMemProducerCharCnt = ", shMemProducerCharCnt);
+	close(fd);
+}
+
+
+void readFromPipe(int consumerCharCount)
+{
+	int c;
+	read(fd_pipe[0], &c, sizeof(int));
+	printBytesWritten("CONSUMER: The producer value of shMemProducerCharCnt = ", c);
+	printBytesWritten("CONSUMER: The producer value of shMemConsumerCharCnt = ", consumerCharCount);	
+}
+
+
+void consume()
+{
+	in = ptr2 + BUFFER_SIZE;
+	out = ptr2 + BUFFER_SIZE + 4;
+	done = ptr2 + BUFFER_SIZE + 8;
+	int shMemConsumerCharCnt = 0;
+	int iters = 1;
+
+	int fd = open(OUTFILENAME, O_CREAT | O_WRONLY);
+
+	char* buff = (char*) malloc(CHUNK_SIZE);
+	int count = 0;
+	
+	for (;;)
+	{
+		while (*in == *out)
+		{
+			if (*done == 1)
+			{	
+				buff -= count;
+				outputToTerminal(1, buff, iters);
+				int res = write(fd, buff, count);
+				if (res < 0)
+				{
+					exit(-2);
+				}
+				readFromPipe(shMemConsumerCharCnt);
+				close(fd);
+				return;	
+			}
+		}
+		*buff++ = *(ptr2 + *out);
+		count++;
+		shMemConsumerCharCnt++;
+		if (count == CHUNK_SIZE)
+		{
+			buff -= count;
+			outputToTerminal(1, buff, iters++);
+			
+			int res = write(fd, buff, count);
+			if (res < 0)
+			{
+				exit(-2);
+			}
+			count = 0;
+			buff = (char *)malloc(CHUNK_SIZE);
+		}
+		*out = (*out + 1) % BUFFER_SIZE;
+	}
+
+}
+
+
+	//	UTILITY FUNCTION DEFINITION STARTS HERE
+
+
+int getFileNameSizeWithOutExtension(char* name)
+{
+	int size = getBufferSize(name);
+	for (int i = 1; i < size; i++)
+	{
+		if (*(name + i) == '.')
+			return i;
+	}
+	return size;
+}
+
+
+char* getOutputFileName(char* name, char* extension)
+{
+	int nameSize = getFileNameSizeWithOutExtension(name);
+	printf("%d\n", nameSize);
+	int extensionSize = getBufferSize(extension);
+	int totalSize = nameSize + extensionSize + 1;
+	
+	char* nameWithExtension = (char *)malloc(totalSize);
+	nameWithExtension = strncat(nameWithExtension, name, nameSize);
+	nameWithExtension = strcat(nameWithExtension, ".");
+	nameWithExtension = strcat(nameWithExtension, extension);
+
+	return nameWithExtension;
+}
+
+
+void writeOutput(char* buff, int len)
+{
+	write(STDOUT_FILENO, buff, len);
+	write(output_file, buff, len);
 }
 
 
@@ -97,6 +260,25 @@ int getBufferSize(char* buff)
 }
 
 
+void printBytesWritten(char* content,int producerBytes)
+{
+	//char* content = "PRODUCER: The producer value of shMemProducerCharCnt = ";
+	int bufferSize = getBufferSize(content);
+	char* number = numberToChar(producerBytes);
+	int digitsCount = countDigits(producerBytes);
+
+	int totalBufferSize = bufferSize + digitsCount + 1;
+
+	char* buffer = (char*)malloc(totalBufferSize);
+
+	buffer = strcat(buffer, content);
+	buffer = strcat(buffer, number);
+	buffer = strcat(buffer, "\n");
+
+	writeOutput(buffer, totalBufferSize);
+}
+
+
 void outputToTerminal(int who, char* buff, int index)
 {
 	char* parent = "PARENT: ";
@@ -126,91 +308,6 @@ void outputToTerminal(int who, char* buff, int index)
 	buff1 = strcat(buff1, _item);
 	buff1 = strcat(buff1, buff);
 	buff1 = strcat(buff1, "\n");
-	write(STDOUT_FILENO, buff1, totalBuffSize);
+	writeOutput(buff1, totalBuffSize);
 }
 
-
-void produce()
-{
-	in = ptr1 + BUFFER_SIZE;
-	out = ptr1 + BUFFER_SIZE + 4;
-	done = ptr1 + BUFFER_SIZE + 8;
-	*in = 0;
-	*out = 0;
-	*done = 0;
-	int iters = 1;
-
-	char* buff = (char*)malloc(CHUNK_SIZE);
-
-	int fd = open(SRCFILENAME, O_RDONLY);
-	int read_bytes = read(fd, buff, CHUNK_SIZE);
-
-	while (read_bytes > 0)
-	{
-		*(buff + read_bytes) = 0x00;
-		for (int i = 0; i < read_bytes; i++)
-		{
-			while ((*in + 1) % BUFFER_SIZE == *out)
-			{
-
-			}
-			*(ptr1 + *in) = (char)(*buff++);
-			*in = (*in + 1) % BUFFER_SIZE;
-		}
-		buff -= read_bytes;
-		outputToTerminal(0, buff, iters++);
-		read_bytes = read(fd, buff, CHUNK_SIZE);
-	}
-	*done = 1;
-	close(fd);
-}
-
-void consume()
-{
-	in = ptr2 + BUFFER_SIZE;
-	out = ptr2 + BUFFER_SIZE + 4;
-	done = ptr2 + BUFFER_SIZE + 8;
-
-	int iters = 1;
-
-	int fd = open(OUTFILENAME, O_CREAT | O_WRONLY);
-
-	char* buff = (char*) malloc(CHUNK_SIZE);
-	int count = 0;
-	
-	for (;;)
-	{
-		while (*in == *out)
-		{
-			if (*done == 1)
-			{	
-				buff -= count;
-				outputToTerminal(1, buff, iters);
-				int res = write(fd, buff, count);
-				if (res < 0)
-				{
-					exit(-2);
-				}
-				close(fd);
-				return;	
-			}
-		}
-		*buff++ = *(ptr2 + *out);
-		count++;
-		if (count == CHUNK_SIZE)
-		{
-			buff -= count;
-			outputToTerminal(1, buff, iters++);
-			
-			int res = write(fd, buff, count);
-			if (res < 0)
-			{
-				exit(-2);
-			}
-			count = 0;
-			buff = (char *)malloc(CHUNK_SIZE);
-		}
-		*out = (*out + 1) % BUFFER_SIZE;
-	}
-
-}
