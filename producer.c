7f78a1ee66da96abdc4976c68472c92c97d205de
Abin-k-Rajan <abin27577@gmail.com>
2022-03-27 21:35:42 +0530

    simple ipc

--- producer.c
+++ producer.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <string.h>
 
 char *ptr1, *ptr2;
 int* in, *out, *done;
@@ -59,10 +60,73 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-void print(int num)
-{
 
-	printf("%d\n", num);
+int countDigits(int num)
+{
+	int count = 0;
+	while (num)
+	{
+		count += 1;
+		num /= 10;
+	}
+	return count;
+}
+
+
+
+char* numberToChar(int num)
+{
+	int size = countDigits(num);
+	char* buff = (char *)malloc(size);
+	for(int i = 0; i < size; i++)
+	{
+		*(buff + size - i - 1) = (char)(num % 10 + 48);
+		num /= 10;
+	}
+	return buff;
+}
+
+int getBufferSize(char* buff)
+{
+	int size = 0;
+	while (*(buff + size) != 0)
+	{
+		size++;
+	}
+	return size;
+}
+
+
+void outputToTerminal(int who, char* buff, int index)
+{
+	char* parent = "PARENT: ";
+	char* child = "CHILD: ";
+
+	char* _in = "IN = ";
+	char* _item = "ITEM = ";
+	char* number = numberToChar(index);
+
+	int digitsCount = countDigits(index);
+	int bufferSize = getBufferSize(buff);
+
+
+	int buff1Size = getBufferSize((who == 0 ? parent : child)) + getBufferSize(_in) + digitsCount + 1;
+	int buff2Size = getBufferSize((who == 0 ? parent : child)) + getBufferSize(_item) + bufferSize + 1;
+
+	int totalBuffSize = buff1Size + buff2Size;
+
+	char* buff1 = (char *)malloc(totalBuffSize);
+
+	buff1 = strcat(buff1, (who == 0 ? parent : child));
+	buff1 = strcat(buff1, _in);
+	buff1 = strcat(buff1, number);
+	buff1 = strcat(buff1, "\n");
+
+	buff1 = strcat(buff1, (who == 0 ? parent : child));
+	buff1 = strcat(buff1, _item);
+	buff1 = strcat(buff1, buff);
+	buff1 = strcat(buff1, "\n");
+	write(STDOUT_FILENO, buff1, totalBuffSize);
 }
 
 
@@ -74,17 +138,31 @@ void produce()
 	*in = 0;
 	*out = 0;
 	*done = 0;
+	int iters = 1;
 
-	for (int i = 0; i < 26; i++)
+	char* buff = (char*)malloc(CHUNK_SIZE);
+
+	int fd = open(SRCFILENAME, O_RDONLY);
+	int read_bytes = read(fd, buff, CHUNK_SIZE);
+
+	while (read_bytes > 0)
 	{
-		while ((*in + 1) % BUFFER_SIZE == *out)
+		*(buff + read_bytes) = 0x00;
+		for (int i = 0; i < read_bytes; i++)
 		{
+			while ((*in + 1) % BUFFER_SIZE == *out)
+			{
 
+			}
+			*(ptr1 + *in) = (char)(*buff++);
+			*in = (*in + 1) % BUFFER_SIZE;
 		}
-		*(ptr1 + *in) = (char)(97 + i);
-		*in = (*in + 1) % BUFFER_SIZE;
+		buff -= read_bytes;
+		outputToTerminal(0, buff, iters++);
+		read_bytes = read(fd, buff, CHUNK_SIZE);
 	}
 	*done = 1;
+	close(fd);
 }
 
 void consume()
@@ -92,6 +170,10 @@ void consume()
 	in = ptr2 + BUFFER_SIZE;
 	out = ptr2 + BUFFER_SIZE + 4;
 	done = ptr2 + BUFFER_SIZE + 8;
+
+	int iters = 1;
+
+	int fd = open(OUTFILENAME, O_CREAT | O_WRONLY);
 
 	char* buff = (char*) malloc(CHUNK_SIZE);
 	int count = 0;
@@ -103,14 +185,32 @@ void consume()
 			if (*done == 1)
 			{	
 				buff -= count;
-				printf("%s\n", buff);
+				outputToTerminal(1, buff, iters);
+				int res = write(fd, buff, count);
+				if (res < 0)
+				{
+					exit(-2);
+				}
+				close(fd);
 				return;	
 			}
 		}
 		*buff++ = *(ptr2 + *out);
 		count++;
+		if (count == CHUNK_SIZE)
+		{
+			buff -= count;
+			outputToTerminal(1, buff, iters++);
+			
+			int res = write(fd, buff, count);
+			if (res < 0)
+			{
+				exit(-2);
+			}
+			count = 0;
+			buff = (char *)malloc(CHUNK_SIZE);
+		}
 		*out = (*out + 1) % BUFFER_SIZE;
 	}
 
-	printf("%s\n", buff);	
 }
